907ecc6bd8bf8b747b394cad392751db6cc23341
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-09-07 19:23:50 +0900

    큐2 풀이

---
+++ 큐2.cpp
@@ -0,0 +1,29 @@
+﻿#include <queue>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, num;
+	string order;
+	queue <int> que;
+	cin >> N;
+	for ( i = 0; i < N; i++)
+	{
+		cin >> order;
+		if (order == "push") {
+			cin >> num;
+			que.push(num);
+		}
+		else if (order == "pop") {
+			if (!que.empty()) {
+				cout << que.front() << '\n';
+				que.pop();
+			}
+			else cout << -1;
+		}
+		else if (order == "size") cout << que.size() << '\n';
+		else if (order == "empty") cout << (!que.empty() ? 0 : 1) << '\n';
+		else if (order == "front") cout << (!que.empty() ? que.front() : -1) << '\n';
+		else if (order == "back") cout << (!que.empty() ? que.back() : -1) << '\n';
+	}
+}
