89fe037a60fa92c5a053bcd9c9621701736fa3c6
Jean Young Park <70056958+JeanYoungPark@users.noreply.github.com>
2020-09-20 20:28:20 +0900

    다시풀어보기

---
+++ 곱셈.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+long long int fs(long long A, long long B, long long C) {
+	if (!B) return 1;
+	long long val = fs(A,B/2,C);
+	val = val * val * C;
+
+	if (B % 2) return val * A % C;
+	else return val;
+}
+
+int main()
+{
+	long long int A, B, C;
+	cin >> A >> B >> C;
+	cout << fs(A, B, C);
+}
+
+//https://www.acmicpc.net/problem/1629
